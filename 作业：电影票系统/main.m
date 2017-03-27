a5c7bbaeb7387a314adc4a4f8400a60d16419a38
韦钰 <48532140@qq.com>
2017-03-27 19:25:13 +0800

    Initial Commit

---
+++ 作业：电影票系统/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  作业：电影票系统
+//
+//  Created by 韦钰 on 2017/3/27.
+//  Copyright © 2017年 韦钰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
